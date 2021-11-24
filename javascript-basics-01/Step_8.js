89aee80740e8eb1c0f39f0a5684567eaf63ebc79
Zaynab Abd El Nabi <zaynab.abdelnabi@gmail.com>
2021-11-24 18:09:22 +0200

    completed js-basics-01 Step_8

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,9 @@
+function eighteen() {
+    var age = document.getElementById('age').value;
+    console.log(age);
+    if (age >= 18) {
+        alert('you are over 18');
+    } else {
+        alert('you are under 18');
+    }
+}
