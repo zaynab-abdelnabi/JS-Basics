09a151822153ddd1db38d5c7ab66eab8e18851eb
Zaynab Abd El Nabi <zaynab.abdelnabi@gmail.com>
2021-11-24 23:36:40 +0200

    completed js-basics-02 Step_5

---
+++ javascript-basics-02/step 5/Step_5.js
@@ -0,0 +1,20 @@
+function change_img(numb) {
+    switch (numb) {
+        case 1:
+            document.getElementById('image1').src = 'images/image1_2.jpg';
+            break;
+        case 2:
+            document.getElementById('image2').src = 'images/image2_2.jpg';
+            break;
+        case 3:
+            document.getElementById('image3').src = 'images/image3_2.jpg';
+            break;
+        case 4:
+            document.getElementById('image4').src = 'images/image4_2.jpg';
+            break;
+        case 5:
+            document.getElementById('image5').src = 'images/image5_2.jpg';
+            break;
+        default: 
+    }
+};
