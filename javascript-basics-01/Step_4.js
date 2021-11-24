06d3ffb5549fa8e620f7028a52786aa30f0b8e92
Zaynab Abd El Nabi <zaynab.abdelnabi@gmail.com>
2021-11-24 17:17:21 +0200

    completed js-basics-01 Step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,7 @@
+function hello() {
+    var name = document.getElementById('name').value;
+    var surname = document.getElementById('surname').value;
+    var city = document.getElementById('city').value;
+
+    alert('Nom : ' + name + '\nPrenom : ' + surname + '\nVille : ' + city);
+}
