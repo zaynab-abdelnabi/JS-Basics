c5fdf4ebbe70363ae200cbaa8c326a47eeb3a5cb
Zaynab Abd El Nabi <zaynab.abdelnabi@gmail.com>
2021-11-24 23:48:35 +0200

    completed js-basics-03 Step_2

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,7 @@
+function anchorStatus(num) {
+    if (num === 0) {
+        document.getElementById('texte').style.display = "none";
+    } else {
+        document.getElementById('texte').style.display = "block";
+    }
+}
