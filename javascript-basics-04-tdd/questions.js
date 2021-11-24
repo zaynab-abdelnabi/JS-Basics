e4dac011deed2b4e8da94dd61296ba920dbf44ee
Zaynab Abd El Nabi <zaynab.abdelnabi@gmail.com>
2021-11-25 01:30:48 +0200

    completed js-basics-04 half

--- javascript-basics-04-tdd/questions.js
+++ javascript-basics-04-tdd/questions.js
@@ -1,59 +1,59 @@
 
 const stringSize = (text) => {
-
+    return text.length;
 }
 const replaceCharacterE = (text) => {
-
+    return text.replace('e', ' ')
 }
 const concatString = (text1, text2) => {
-
+    return text1.concat(text2)
 }
 const showChar5 = (text) => {
-
+    return text.slice(4, 5)
 }
 const showChar9 = (text) => {
-
+    return text.slice(0, 9)
 }
 const toCapitals = (text) => {
-
+    return text.toUpperCase();
 }
 const toLowerCase = (text) => {
-
+    return text.toLowerCase();
 }
 const removeSpaces = (text) => {
-
+    return text.trim();
 }
 const IsString = (text) => {
-
+    return typeof (text) === 'string';
 }
 
 const getExtension = (text) => {
-
+    return text.split('.').pop();
 }
 const countSpaces = (text) => {
-
+    return text.split(" ").length - 1;
 }
 const InverseString = (text) => {
-
+    return text.split("").reverse().join("");
 }
 
 const power = (x, y) => {
-
+    return Math.pow(x, y);
 }
 const absoluteValue = (num) => {
-
+    return Math.abs(num);
 }
 const absoluteValueArray = (array) => {
-
+    return array.map(Math.abs);
 }
 const circleSurface = (radius) => {
-
+    return Math.round(Math.PI * Math.pow(radius, 2));
 }
 const hypothenuse = (ab, ac) => {
-
+    return Math.sqrt(Math.pow(ab, 2) + Math.pow(ac, 2));
 }
 const BMI = (weight, height) => {
-
+    return  Number((weight / (height * height)).toFixed(2));
 }
 
 const createLanguagesArray = () => {
