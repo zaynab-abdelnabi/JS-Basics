dca4714a8f2af91c31b0894b25f801797d0f21c4
Zaynab Abd El Nabi <zaynab.abdelnabi@gmail.com>
2021-11-24 17:38:13 +0200

    completed js-basics-01 Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,6 @@
+function divide() {
+    var first_number = document.getElementById('first_number').value;
+    var second_number = document.getElementById('second_number').value;
+
+    alert('The reminder is ' + first_number%second_number);
+}
