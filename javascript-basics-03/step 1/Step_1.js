ccc2d515f7d1f0cec018a14fed417b943754975c
Zaynab Abd El Nabi <zaynab.abdelnabi@gmail.com>
2021-11-24 23:40:43 +0200

    completed js-basics-03 Step_1

---
+++ javascript-basics-03/step 1/Step_1.js
@@ -0,0 +1,6 @@
+function addBorder() {
+    document.getElementById('image1').style.border = "3px solid red";
+}
+function removeBorder() {
+    document.getElementById('image1').style.border = "0";
+}
